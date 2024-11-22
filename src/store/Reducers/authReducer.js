7c20102a8d6fec0e727f81a94a82b03ad19ad265
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-22 10:56:07 +0900

    104ch admin-login

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,5 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+
+
+export const admin_login = createAsyncThunk(
+    'auth/admin_login',
+    async(info) => {
+        console.log(info)
+        try {
+            // const {data} = await api.post('/admin-login',info,{withCedentials: true})
+            // console.log(data)
+        } catch (error) {
+            
+        }
+    }
+)
 
 
 

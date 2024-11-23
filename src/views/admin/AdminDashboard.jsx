992278bc54f2dab9792b27ed02683f911645e810
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-23 13:18:12 +0900

    ok

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -4,6 +4,7 @@ import { MdCurrencyExchange } from "react-icons/md";
 import { MdProductionQuantityLimits } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
 import Chart from 'react-apexcharts';
+import { Link } from 'react-router-dom';
 
 
 
@@ -125,7 +126,7 @@ const AdminDashboard = () => {
 
 
              
-        
+            {/* Apex-Chart   ------------*/}
             <div className='w-full flex flex-wrap mt-7'>
                 <div className='w-full lg:w-7/12 lg:pr-3'>
                     <div className='w-full bg-[#6a5fdf] p-4 rounded-md'>
@@ -133,8 +134,97 @@ const AdminDashboard = () => {
                     </div>
 
                 </div>
+            {/* Chat ---------- */}
+                <div className='w-full lg:w-5/12 lg:pl-4 mt-6 lg:mt-0'>
+                    <div className='w-full bg-[#6a5fdf] p-4 rounded-md text-[#d0d2d6]'>
+                        <div className='flex justify-between items-center'>
+                            <h2 className='font-semibold text-lg text-[#d0d2d6] pb-3'>Recent Seller Message</h2>
+                            <Link className='font-semibold text-sm text-[#d0d2d6]'>View All</Link>
+                        </div>
+                <div className='flex flex-col gap-2 pt-6 text-[#d0d2d6]'>
+                    <ol className='relative border-1 border-slate-600 ml-4'>
+                        <li className='mb-3 ml-6'>
+                            <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c7fe2] rounded-full z-10'>
+                            <img className='w-full rounded-full h-full shadow-lg' src="http://localhost:3000/images/admin.jpg" alt="" /></div>
+                            <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                            <div className='flex justify-between items-center mb-2'>
+                        <Link className='text-md font-normal'>Admin</Link>
+                        <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'> 2 day ago</time>
+                            </div>
+                            <div className='p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800'>
+                                How Are you
+                            </div>
+                            </div>
+                        </li>
+                        <li className='mb-3 ml-6'>
+                            <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c7fe2] rounded-full z-10'>
+                            <img className='w-full rounded-full h-full shadow-lg' src="http://localhost:3000/images/admin.jpg" alt="" /></div>
+                            <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                            <div className='flex justify-between items-center mb-2'>
+                        <Link className='text-md font-normal'>Admin</Link>
+                        <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'> 2 day ago</time>
+                            </div>
+                            <div className='p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800'>
+                                How Are you
+                            </div>
+                            </div>
+                        </li>
+                        <li className='mb-3 ml-6'>
+                            <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c7fe2] rounded-full z-10'>
+                            <img className='w-full rounded-full h-full shadow-lg' src="http://localhost:3000/images/admin.jpg" alt="" /></div>
+                            <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                            <div className='flex justify-between items-center mb-2'>
+                        <Link className='text-md font-normal'>Admin</Link>
+                        <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'> 2 day ago</time>
+                            </div>
+                            <div className='p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800'>
+                                How Are you
+                            </div>
+                            </div>
+                        </li>
+                    </ol>
+                </div>
+                    </div>
+                </div>
             </div>
 
+
+            {/* Order  --------------- */}
+            <div className='w-full p-4 bg-[#6a5fdf] rounded-md mt-6'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='font-semibold text-lg text-[#d0d2d6] pb-3 '>Recent Orders</h2>
+                        <Link className='font-semibold text-sm text-[#d0d2d6]'>View All</Link>
+                    </div>
+            <div className='relative overflow-x-auto'>
+            <table className='w-full text-sm text-left text-[#d0d2d6]'>
+                <thead className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
+                <tr>
+                    <th scope='col' className='py-3 px-4'>Order Id</th>
+                    <th scope='col' className='py-3 px-4'>Price</th>
+                    <th scope='col' className='py-3 px-4'>Payment Status</th>
+                    <th scope='col' className='py-3 px-4'>Order Status</th>
+                    <th scope='col' className='py-3 px-4'>Active</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>#34344</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>$454</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>View</th>
+                    </tr>
+                    <tr>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>#34344</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>$454</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</th>
+                        <th scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>View</th>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            </div>
 
 
         </div>

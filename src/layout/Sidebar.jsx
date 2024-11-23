c5c1596d0fb1af441e8aabf2a6a0a58989e46a79
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-23 11:12:08 +0900

    123ch ok

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { getNav } from '../navigation/index';
+import { BiLogOutCircle } from "react-icons/bi";
 
 
 
@@ -40,6 +41,14 @@ const Sidebar = () => {
                                 </Link>
                             </li> )
                         }
+
+                        <li>
+                            <button className='text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1'>
+                            <span><BiLogOutCircle /></span>
+                            <span>Logout</span>
+                            </button>
+                        </li>
+
                     </ul>
 
 

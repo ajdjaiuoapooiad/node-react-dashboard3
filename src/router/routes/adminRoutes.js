a979d5f135d2fb16ba29872e686b631eb3554b6a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-23 14:16:47 +0900

    133ch ok

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,9 +1,19 @@
 import { lazy } from "react";   
+const Orders = lazy(() => import("../../views/admin/Order")); 
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))   
+
+
+
+
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
         element : <AdminDashboard/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/orders',
+        element : <Orders/> ,
         role : 'admin'
     }
 ]

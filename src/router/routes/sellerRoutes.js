4a794ac995dbcf529951d88710fa1b834f1e2d0b
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-24 13:35:51 +0900

    ok

--- src/router/routes/sellerRoutes.js
+++ src/router/routes/sellerRoutes.js
@@ -1,6 +1,9 @@
 import { lazy } from "react";  
 import SellerDashboard from "../../views/seller/SellerDashboard";
+import AddProduct from "../../views/seller/AddProduct";
 const Home = lazy(()=> import('../../views/Home'))   
+
+
 export const sellerRoutes = [
     {
         path: '/',
@@ -10,6 +13,11 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard',
         element : <SellerDashboard/>,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/add-product',
+        element : <AddProduct/>,
         ability : ['seller']
     }
 ]
